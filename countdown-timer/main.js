99218ce31a3b3ccb8a594e64d735dc3116deed22
Salamata Bah <57270552+Salamata4255@users.noreply.github.com>
2021-02-28 11:45:17 -0500

    Add files via upload
    added timer

---
+++ countdown-timer/main.js
@@ -0,0 +1,48 @@
+
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var h = document.getElementById("hour");
+var m = document.getElementById("minute");
+var s = document.getElementById("sec");
+
+var startTimer = null;
+
+start.addEventListener('click', function(){
+
+    function startInterval(){
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+    }
+    startInterval();
+})
+
+reset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+
+    stopInterval()
+})
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    }
+    return;
+}
+
+
+function stopInterval() {
+    clearInterval(startTimer);
+}
