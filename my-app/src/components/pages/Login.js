4315c3c2bdcab2e2c72a91fb619be3c2f5029410
tam389 <tam389@drexel.edu>
2021-02-28 13:07:27 -0500

    revise authentication

--- my-app/src/components/pages/Login.js
+++ my-app/src/components/pages/Login.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import './Login.css'
 import { Button } from '../Button'
+import firebase from 'firebase'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAeUw2MfTi7gpYy2Bt-YelHeeJMhEd1sQ0',
+  authDomain: 'techtogether-auth.firebaseapp.com',
+})
 
 function Login() {
+  React.state = { isSignedIn: false }
+  React.uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  }
+  const componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      React.setState({ isSignedIn: !!user })
+    })
+  }
   return (
     <main id="Login">
       <div className="login-container">
@@ -11,13 +31,14 @@ function Login() {
           <i>Let us help you have a productive day</i>
         </p>
         <div className="button-groups">
-          <Button
-            text={'Signin with Google'}
-            color={'btn-lilac'}
-            buttonSize={'btn-large'}
-            icon={'fa fa-google'}
-            dest={'/'}
-          />
+          {React.state.isSignedIn ? (
+            <div>Signed In!</div>
+          ) : (
+            <StyledFirebaseAuth
+              uiConfig={React.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )}
           <Button
             text={'About us'}
             color={'btn-lilac'}
